944b17dde9a3aa9dac47ca3237741241bc9bde77
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-10 12:34:53 -0500

    middleware added

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 'use strict';
 
-// technologies and paths needed //
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/entry');
+
 const app = express();
-const { knex } = require('./db/database');
 
-
-// give the app access to the routes folder //
-const routes = require('./routes/');
-
-
-// Port //
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(routes);
+app.use((req, res) => {
+  res.render('entry')
+});
+
+app.listen(port, function() {
+  console.log(`Serving on ${port}`)
 });
 
 module.exports = app;
