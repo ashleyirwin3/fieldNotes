f201c2ef3e40dd259b13b66dc0eb4e1bcf862dc2
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-10 12:35:23 -0500

    changed getAll() to fetchAll()

--- controllers/entryCtrl.js
+++ controllers/entryCtrl.js
@@ -4,7 +4,7 @@ const { knex } = require('../db/database');
 const Entry = require('../models/entry');
 
 module.exports.getEntries = (req, res, next) => {
-  Entry.getAll()
+  Entry.fetchAll()
   .then( (entries) => {
     res.status(200).json(entries);
   });
