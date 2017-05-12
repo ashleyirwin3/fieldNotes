31fc860d06ce6effdab9e9c6e7e3faaa1625a88d
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-12 10:05:58 -0500

    added router.post for create function

--- routes/entry.js
+++ routes/entry.js
@@ -2,10 +2,12 @@
 
 const { Router } = require('express');
 
-const { getEntries } = require('../controllers/entryCtrl');
+const { show, create, getEntries } = require('../controllers/entryCtrl');
 
 const router = Router();
 
+router.get('/entry', show);
+router.post('/entry', create);
 router.get('/entry', getEntries);
 
 module.exports = router;
