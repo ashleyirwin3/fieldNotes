04ea377dbc14eccc615d1b07a15f797da15864ee
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-12 11:02:07 -0500

    added router.get for show function and router.post for create function

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@
 
 const { Router } = require('express');
 
-const { getUsers } = require('../controllers/userCtrl');
+const { show, create, getUsers } = require('../controllers/userCtrl');
 
 const router = Router();
 
+router.get('/user', show);
+router.post('/user', create);
 router.get('/user', getUsers);
+
 
 module.exports = router;
