f4cfe67ddcc4a967f6168eac0976ed7f5ab44892
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-12 11:01:33 -0500

    added show and create functions

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,6 +3,19 @@
 const { knex } = require('../db/database');
 const User = require('../models/user');
 
+module.exports.show = (req, res) => {
+  res.render('user')
+}
+
+module.exports.create = (req, res, err) => {
+  console.log(req.body)
+  User.forge(req.body)
+    .save()
+    .then((userObject) => {
+      console.log(req.body)
+    });
+};
+
 module.exports.getUsers = (req, res, next) => {
   User.fetchAll()
   .then( (users) => {
