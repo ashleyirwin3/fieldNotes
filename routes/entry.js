536fb5caa7bd7ef454623d9ffdbfb3521e22c608
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-15 13:59:57 -0500

    commented out additional router.get

--- routes/entry.js
+++ routes/entry.js
@@ -2,12 +2,12 @@
 
 const { Router } = require('express');
 
-const { show, create, getEntries } = require('../controllers/entryCtrl');
+const { show, create } = require('../controllers/entryCtrl');
 
 const router = Router();
 
 router.get('/entry', show);
 router.post('/entry', create);
-router.get('/entry', getEntries);
+// router.get('/entry', getEntries);
 
 module.exports = router;
