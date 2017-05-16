203bdb0b365e0dc7c14f33d03d09462cda327e13
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-16 11:44:48 -0500

    added queries for filtering through entries

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -5,7 +5,28 @@ const Profile = require('../models/profile');
 
 
 module.exports.getEntries = (req, res, next) => {
-  Profile.getAllEntries()
+  Profile.fetchAll()
     .then(entries => res.status(200).json(entries))
     .catch(err => next(err))
+}
+
+module.exports.getEntryByDate = ({ params: { date } }, res, next) => {
+  Profile.forge({ date })
+  .fetchAll()
+  .then(entries => res.status(200).json(entries))
+  .catch(err => next(err))
+}
+
+module.exports.getEntryByPerson = ({ params: { surveyors } }, res, next) => {
+  Profile.forge({ surveyors })
+  .fetchAll()
+  .then(entries => res.status(200).json(entries))
+  .catch(err => next(err))
+}
+
+module.exports.deleteEntry = ({ params: { id } }, res, next) => {
+  Profile.forge({ id })
+  .destroy()
+  .then(() => res.status(201).json({ "msg": "Entry Deleted"}))
+  .catch(err => next(err))
 }
