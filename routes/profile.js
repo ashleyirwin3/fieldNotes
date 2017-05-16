7527c42afa879ebd6dfc435f77dbb9d67470cb8e
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-16 15:53:00 -0500

    added router.post

--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,14 @@
 
 const { Router } = require('express');
 
-const { getEntries, getEntryByDate, getEntryByPerson, deleteEntry } = require('../controllers/profileCtrl');
+const { getEntries, addEntry, getEntryByDate, getEntryByPerson, deleteEntry } = require('../controllers/profileCtrl');
 
 const router = Router();
 
-// router.get('/profile', show);
 router.get('/profile', getEntries)
+router.post('/profile', addEntry)
 router.get('/profile/:date', getEntryByDate)
 router.get('/profile/:surveyors', getEntryByPerson)
-router.delete('/profile/:d', deleteEntry)
+router.delete('/profile/:id', deleteEntry)
 
 module.exports = router;
