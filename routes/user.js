ed819290fb498c251fbc0e59ddcedcb4e49c6232
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-16 11:48:33 -0500

    deleted show function

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,10 @@
 
 const { Router } = require('express');
 
-const { show, create, getUsers } = require('../controllers/userCtrl');
+const { create, getUsers } = require('../controllers/userCtrl');
 
 const router = Router();
 
-router.get('/user', show);
 router.post('/user', create);
 router.get('/user', getUsers);
 
